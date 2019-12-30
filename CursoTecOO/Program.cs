67471e8f4e6711eb6b7e393b7fec60cd57c4f4ea
Inhangá <nakappa1@hotmail.com>
2019-12-30 11:53:07 -0300

    Iniciando prejeto e criando a classe CentralDeExercicios

---
+++ CursoTecOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursoTecOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>()
+            {
+            });
+        }
+    }
+}
