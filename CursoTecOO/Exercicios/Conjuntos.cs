e51b72ec905eaafb1098b3e60d4c361d5ddefc28
Inhangá <nakappa1@hotmail.com>
2019-12-30 14:43:44 -0300

    Exercicio de Conjuntos

---
+++ CursoTecOO/Exercicios/Conjuntos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoTecOO.Exercicios
+{
+    class Conjuntos
+    {
+        public static void Executar()
+        {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            Console.Write("O curso A possui quantos alunos? ");
+            int quantAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso A: ");
+            for (int i = 0; i < quantAlunos; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                A.Add(x);
+            }
+
+            Console.WriteLine();
+            Console.Write("O curso B possui quantos alunos? ");
+            quantAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso B: ");
+            for (int i = 0; i < quantAlunos; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                B.Add(x);
+            }
+
+            Console.WriteLine();
+            Console.Write("O curso C possui quantos alunos? ");
+            quantAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso C: ");
+            for (int i = 0; i < quantAlunos; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                C.Add(x);
+            }
+
+            HashSet<int> Novo = new HashSet<int>();
+            Novo.UnionWith(A);
+            Novo.UnionWith(B);
+            Novo.UnionWith(C);
+
+            Console.WriteLine();
+            Console.WriteLine("Total de Alunos: " + Novo.Count);
+        }
+    }
+}
--- CursoTecOO/Program.cs
+++ CursoTecOO/Program.cs
@@ -13,6 +13,7 @@ namespace CursoTecOO
                 {  "Vetores - Técnicas de orientação a objetos e estruturas de dados", Vetores.Executar  },
                 {  "Matrizes - Técnicas de orientação a objetos e estruturas de dados", Matrizes.Executar  },
                 {  "Listas - Técnicas de orientação a objetos e estruturas de dados", Listas.Executar  },
+                {  "Conjuntos - Técnicas de orientação a objetos e estruturas de dados", Conjuntos.Executar  },
             });
             
             central.SelecionarEExecutar();
